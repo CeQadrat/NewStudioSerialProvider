4cb23944d86dcbb847aad36d064c39df55f3a9ef
Dima Naumchik <ceqadrat@gmail.com>
2016-10-02 18:19:36 +0300

    create series generator

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const getPage = require('./getPageRequest');
 const serialListParser = require('./parsers/newStudioSerialListParser');
 const searchGenerator = require('./generators/newStudioSearchGenerator');
+const episodeGenerator = require('./generators/newStudioSeriesGenerator');
 const episodeParser = require('./parsers/newStudioEpisodeParser');
-const  co = require('co');
+const co = require('co');
 
 // getPage('http://newstudio.tv/').then((body) => {
 //     for(let serial of serialListParser.parse(body)){
@@ -10,10 +11,16 @@ const  co = require('co');
 //     }
 // });
 
-// co(searchGenerator('http://newstudio.tv/viewforum.php?f=394')).then((episodes) => {
-//     console.log(episodes);
-// }).catch(console.error);
+co(searchGenerator('http://newstudio.tv/viewforum.php?f=394')).then((episodes) => {
+    let generator = episodeGenerator(episodes);
+    generator.next().value.then((ep) => {
+        console.log(ep);
+        generator.next().value.then((ep) => {
+            console.log(ep);
+        });
+    });
+}).catch(console.error);
 
-getPage('http://newstudio.tv/viewtopic.php?t=20761').then((body) => {
-    console.log(episodeParser.parse(body));
-});
+// getPage('http://newstudio.tv/viewtopic.php?t=20761').then((body) => {
+//     console.log(episodeParser.parse(body));
+// });
--- parsers/newStudioEpisodeParser.js
+++ parsers/newStudioEpisodeParser.js
@@ -12,63 +12,66 @@ module.exports = {
         let enableParseName = false;
         let enableParseInfo = false;
         let episode = {};
-        episode.torrent = {
+        episode.torrentLinks = [];
+        let torrent = {
             link: '',
             info: ''
         };
         let parser = new htmlParser.Parser({
             onopentag: (name, attribs) => {
-                if(name == 'var' && attribs.class == 'postImg' && !episode.serialCover){
+                if (name == 'var' && attribs.class == 'postImg' && !episode.serialCover) {
                     episode.serialCover = attribs.title;
                 }
-                if(name == 'span' && (attribs.class == 'post-b' || attribs.class == 'post-u')){
+                if (name == 'span' && (attribs.class == 'post-b' || attribs.class == 'post-u')) {
                     enableParseText = true;
                 }
-                if(name == 'a' && attribs.class == 'seedmed'){
-                    episode.torrent.link = 'http://newstudio.tv/' + attribs.href;
+                if (name == 'a' && attribs.class == 'seedmed') {
+                    torrent.link = 'http://newstudio.tv/' + attribs.href;
+                    episode.torrentLinks.push(torrent);
+                    torrent = {};
                 }
             },
             ontext: (text) => {
-                if(enableParseSerialName){
+                if (enableParseSerialName) {
                     episode.serialName = text;
                     enableParseSerialName = false;
                 }
-                if(enableParseText && text == 'Сериал:'){
+                if (enableParseText && text == 'Сериал:') {
                     enableParseSerialName = true;
                 }
-                if(enableParseSeason){
-                    episode.season = text.slice(1,-2);
+                if (enableParseSeason) {
+                    episode.season = text.slice(1, -2);
                     enableParseSeason = false;
                 }
-                if(enableParseText && text == 'Сезон:'){
+                if (enableParseText && text == 'Сезон:') {
                     enableParseSeason = true;
                 }
-                if(enableParseSeries){
+                if (enableParseSeries) {
                     episode.series = text.slice(1);
                     enableParseSeries = false;
                 }
-                if(enableParseText && text == 'Серия:'){
+                if (enableParseText && text == 'Серия:') {
                     enableParseSeries = true;
                 }
-                if(enableParseName){
+                if (enableParseName) {
                     episode.name = text.slice(1);
                     enableParseName = false;
                 }
-                if(enableParseText && text == 'Название серии:'){
+                if (enableParseText && text == 'Название серии:') {
                     enableParseName = true;
                 }
-                if(enableParseText && text == 'Релиз группы'){
+                if (enableParseText && text == 'Релиз группы') {
                     enableParseInfo = false;
                 }
-                if(enableParseInfo){
-                    episode.torrent.info += text+' ';
+                if (enableParseInfo) {
+                    torrent.info += text + ' ';
                 }
-                if(enableParseText && text == 'Файл'){
+                if (enableParseText && text == 'Файл') {
                     enableParseInfo = true;
                 }
             },
             onclosetag: (tagname) => {
-                if(tagname == 'span' && enableParseText){
+                if (tagname == 'span' && enableParseText) {
                     enableParseText = false;
                 }
             }
