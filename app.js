e8aaf13123db9ee693f4b2b9145a3334b502cf02
Dima Naumchik <ceqadrat@gmail.com>
2016-10-02 20:17:47 +0300

    make serial provider

--- app.js
+++ app.js
@@ -1,26 +1,9 @@
-const getPage = require('./getPageRequest');
-const serialListParser = require('./parsers/newStudioSerialListParser');
-const searchGenerator = require('./generators/newStudioSearchGenerator');
-const episodeGenerator = require('./generators/newStudioSeriesGenerator');
-const episodeParser = require('./parsers/newStudioEpisodeParser');
-const co = require('co');
+const NewStudioSP = require('./NewStudioSerialProvider');
 
-// getPage('http://newstudio.tv/').then((body) => {
-//     for(let serial of serialListParser.parse(body)){
-//         console.log(serial);
-//     }
-// });
-
-co(searchGenerator('http://newstudio.tv/viewforum.php?f=394')).then((episodes) => {
-    let generator = episodeGenerator(episodes);
-    generator.next().value.then((ep) => {
-        console.log(ep);
-        generator.next().value.then((ep) => {
-            console.log(ep);
-        });
+let provider = new NewStudioSP('мистер робот');
+provider.init().then(() => {
+    let generator = provider.getSeries();
+    generator.next().value.then((episode) => {
+        console.log(episode);
     });
 }).catch(console.error);
-
-// getPage('http://newstudio.tv/viewtopic.php?t=20761').then((body) => {
-//     console.log(episodeParser.parse(body));
-// });
--- generators/newStudioSeriesGenerator.js
+++ generators/newStudioSeriesGenerator.js
@@ -3,25 +3,25 @@ const getPage = require('../getPageRequest');
 const co = require('co');
 
 module.exports = function* getEpisode(series) {
-    for(let episode of series) {
-        yield new Promise((resolve,reject) => {
-            co(function* () {
+    for (let episode of series) {
+        yield new Promise((resolve, reject) => {
+            co(function*() {
                 let series = {};
                 series.torrentLinks = [];
-                for(let link of episode.links){
+                for (let link of episode.links) {
                     let page = yield getPage(link);
                     let ep = episodeParser.parse(page);
-                    for(let key in ep){
-                        if(!series[key] && key != 'torrentLinks'){
+                    for (let key in ep) {
+                        if (!series[key] && key != 'torrentLinks') {
                             series[key] = ep[key];
                         }
                     }
                     series.torrentLinks = series.torrentLinks.concat(ep.torrentLinks);
                 }
                 return series;
-            }).then((episode) => {
-                resolve(episode);
-            }).catch(console.error);
+            })
+                .then((episode) => resolve(episode))
+                .catch(reject);
         });
     }
 };
