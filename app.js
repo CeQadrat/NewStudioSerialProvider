13fde708b2008d58dad5a77ad256a9e3357ef498
Dima Naumchik <ceqadrat@gmail.com>
2016-10-02 17:28:33 +0300

    create episode parser

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const getPage = require('./getPageRequest');
 const serialListParser = require('./parsers/newStudioSerialListParser');
 const searchGenerator = require('./generators/newStudioSearchGenerator');
+const episodeParser = require('./parsers/newStudioEpisodeParser');
 const  co = require('co');
 
 // getPage('http://newstudio.tv/').then((body) => {
@@ -9,6 +10,10 @@ const  co = require('co');
 //     }
 // });
 
-co(searchGenerator('http://newstudio.tv/viewforum.php?f=394')).then((episodes) => {
-    console.log(episodes);
-}).catch(console.error);
+// co(searchGenerator('http://newstudio.tv/viewforum.php?f=394')).then((episodes) => {
+//     console.log(episodes);
+// }).catch(console.error);
+
+getPage('http://newstudio.tv/viewtopic.php?t=20761').then((body) => {
+    console.log(episodeParser.parse(body));
+});
--- parsers/newStudioEpisodeParser.js
+++ parsers/newStudioEpisodeParser.js
@@ -5,18 +5,27 @@ module.exports = {
         return this._parse(page);
     },
     _parse(page){
-        let enableParse = false;
         let enableParseText = false;
         let enableParseSerialName = false;
+        let enableParseSeason = false;
+        let enableParseSeries = false;
+        let enableParseName = false;
+        let enableParseInfo = false;
         let episode = {};
+        episode.torrent = {
+            link: '',
+            info: ''
+        };
         let parser = new htmlParser.Parser({
             onopentag: (name, attribs) => {
-                if(name == 'var' && attribs.class == 'postImg'){
-                    enableParse = true;
+                if(name == 'var' && attribs.class == 'postImg' && !episode.serialCover){
                     episode.serialCover = attribs.title;
                 }
-                if(name == 'span' && attribs.class == 'post-b' && enableParse){
+                if(name == 'span' && (attribs.class == 'post-b' || attribs.class == 'post-u')){
                     enableParseText = true;
+                }
+                if(name == 'a' && attribs.class == 'seedmed'){
+                    episode.torrent.link = 'http://newstudio.tv/' + attribs.href;
                 }
             },
             ontext: (text) => {
@@ -26,6 +35,36 @@ module.exports = {
                 }
                 if(enableParseText && text == 'Сериал:'){
                     enableParseSerialName = true;
+                }
+                if(enableParseSeason){
+                    episode.season = text.slice(1,-2);
+                    enableParseSeason = false;
+                }
+                if(enableParseText && text == 'Сезон:'){
+                    enableParseSeason = true;
+                }
+                if(enableParseSeries){
+                    episode.series = text.slice(1);
+                    enableParseSeries = false;
+                }
+                if(enableParseText && text == 'Серия:'){
+                    enableParseSeries = true;
+                }
+                if(enableParseName){
+                    episode.name = text.slice(1);
+                    enableParseName = false;
+                }
+                if(enableParseText && text == 'Название серии:'){
+                    enableParseName = true;
+                }
+                if(enableParseText && text == 'Релиз группы'){
+                    enableParseInfo = false;
+                }
+                if(enableParseInfo){
+                    episode.torrent.info += text+' ';
+                }
+                if(enableParseText && text == 'Файл'){
+                    enableParseInfo = true;
                 }
             },
             onclosetag: (tagname) => {
